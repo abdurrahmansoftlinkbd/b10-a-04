017c8a86defb8a73eb497c3dfd6d19dbf8910230
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-09-11 21:45:26 +0600

    problem done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function calculateTax(income, expenses) {
   const tax = difference * 0.2;
   return tax;
 }
+console.log(calculateTax(-5000, 2000));
 
 function sendNotification(email) {
   if (!email.includes("@")) {
@@ -31,4 +32,35 @@ function checkDigitsInName(name) {
       return false;
     }
   }
+}
+
+function calculateFinalScore(obj) {
+  if (typeof obj !== "object") {
+    return "Invalid Input";
+  }
+  let result = obj.testScore + obj.schoolGrade;
+  if (obj.isFFamily) {
+    result += 20;
+  }
+
+  if (result >= 80) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function waitingTime(waitingTimes, serialNumber) {
+  if (!Array.isArray(waitingTimes) || typeof serialNumber !== "number") {
+    return "Invalid Input";
+  }
+  let sum = 0;
+  for (let waitingTime of waitingTimes) {
+    sum += waitingTime;
+  }
+  let avg = Math.round(sum / waitingTimes.length);
+  let serial = serialNumber - 1;
+  let beforeSerial = serial - waitingTimes.length;
+  let result = beforeSerial * avg;
+  return result;
 }
